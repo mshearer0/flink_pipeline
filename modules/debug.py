9a80ff3d5662abdd0be8a8a9a63b841c14595edc
Michael Shearer <mshearer@btinternet.com>
2026-01-09 19:01:50 +0000

    Remove accidentally tracked extras

--- modules/debug.py
+++
@@ -1,5 +0,0 @@
-import mgp
-
-@mgp.read_proc
-def check() -> mgp.Record(status=str):
-    return mgp.Record(status="Internal Bridge Working")
--- modules/simple_ingest.py
+++
@@ -1,5 +0,0 @@
-import mgp
-
-@mgp.read_proc
-def hello() -> mgp.Record(greeting=str):
-    return mgp.Record(greeting="Hello from Python!")
